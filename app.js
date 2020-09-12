df45aba2afc88f265ebd42958820dae27eb67372
icedike <icedike@gmail.com>
2020-09-12 21:08:01 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+
+const port = 3000
+
+app.engine('handlebars', exphbs())
+app.set('view engin', 'handlebars')
+
+app.listen(port, () => {
+  console.log(`App is listening on port: ${port}`)
+})
