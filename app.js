2ef1645d0f454aff5958df5846512931bd3492c8
icedike <icedike@gmail.com>
2020-09-13 18:29:11 +0800

    install express-session and connect-flash & confirm user password and unique email

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
 const app = express()
 
@@ -8,9 +10,19 @@ const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
 
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.listen(port, () => {
   db.sequelize.sync()
   console.log(`App is listening on port: ${port}`)
