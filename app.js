c36c7bb174b1014fa3eaef222ab6b3703ccb782c
icedike <icedike@gmail.com>
2020-09-13 16:47:32 +0800

    Create user model

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const db = require('./models')
 const app = express()
 
 const port = 3000
 
-app.engine('handlebars', exphbs())
-app.set('view engin', 'handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`App is listening on port: ${port}`)
 })
+
+require('./routes')(app)
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const restController = require('../controllers/restController')
+const adminController = require('../controllers/restController')
+
+module.exports = app => {
+  app.get('/', (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', restController.getRestaurants)
+
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', adminController.getRestaurants)
+}
