0921fbb7df0cef99d894548e887dd92ac9f1effc
icedike <icedike@gmail.com>
2020-09-13 17:47:51 +0800

    install bcryptjs and body-parser & add userController and signup page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models')
 const app = express()
 
@@ -7,6 +8,8 @@ const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   db.sequelize.sync()
