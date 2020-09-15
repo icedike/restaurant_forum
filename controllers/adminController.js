7825f4ac3bb05cda0af46c9d82850931bef5e2aa
icedike <icedike@gmail.com>
2020-09-15 19:15:16 +0800

    Add admin restaurant page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,6 +8,12 @@ const adminController = {
     })
   },
 
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
+      return res.render('admin/restaurant', { restaurant })
+    })
+  },
+
   createRestaurant: (req, res) => {
     return res.render('admin/create')
   },
