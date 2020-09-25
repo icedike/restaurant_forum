421259de9b1a219d5e125cfd5fec48411032f89e
icedike <icedike@gmail.com>
2020-09-25 20:49:23 +0800

    refactor GET /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const adminController = require('../controllers/api/adminController')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
+router.get('/admin/restaurants/:id', adminController.getRestaurant)
+// router.get('/admin/categories', categoryController.getCategories)
 
 module.exports = router
--- services/adminService.js
+++ services/adminService.js
@@ -5,6 +5,14 @@ const adminService = {
     return Restaurant.findAll({ raw: true, nest: true, include: [Category] }).then(restaurants => {
       callback({ restaurants })
     })
+  },
+  getRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
+      .then(restaurant => {
+        callback({ restaurant: restaurant.toJSON() })
+      })
   }
 }
 module.exports = adminService
