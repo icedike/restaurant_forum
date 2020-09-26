5364b86c5610b4adcc3b36782c84151d3de9b5bc
icedike <icedike@gmail.com>
2020-09-26 18:40:52 +0800

    PUT /api/admin/restaurants/:id

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -6,16 +6,25 @@ const adminController = {
       return res.json(data)
     })
   },
+
   getRestaurant: (req, res) => {
     adminService.getRestaurant(req, res, (data) => {
       return res.json(data)
     })
   },
+
   postRestaurant: (req, res) => {
     adminService.postRestaurant(req, res, (data) => {
       return res.json(data)
     })
   },
+
+  putRestaurant: (req, res) => {
+    adminService.putRestaurant(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+
   deleteRestaurant: (req, res) => {
     adminService.deleteRestaurant(req, res, (data) => {
       return res.json(data)
